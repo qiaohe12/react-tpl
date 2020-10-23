e96e6c78069c47fae7ff5a98fc71fa786faa1723
wanqiao <wanqiao@pinduoduo.com>
2020-10-23 21:18:49 +0800

    fix(bugfix): bugfix

--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { projectName } from '../../Lib/Constant';
 
 const Header = () => {
     return <div className="layout-header">
-        {projectName}
+        {{projectName}}
     </div>
 }
 
